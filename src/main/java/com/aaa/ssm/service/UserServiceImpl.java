76a2e907d138b97c94cadbc12a8a1c3203d295fb
liubin <1615165525@qq.com>
2019-03-09 11:43:43 +0800

    首次提交项目ssm

---
+++ src/main/java/com/aaa/ssm/service/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.aaa.ssm.service;
+
+import com.aaa.ssm.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * fileName:UserServiceImpl
+ * description:
+ * author:zz
+ * createTime:2019/3/2 16:42
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public List<Map> getUserList() {
+            return userDao.getUserList();
+    }
+}
